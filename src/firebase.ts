0fe9b63cf783f4100ad66c1fbb7f2880605fb713
Serhii Karavashkin <ksawl@narod.ru>
2022-07-01 09:36:20 +0300

    add firebase

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBOBTtGdgy6UbLtFqXzBuf4Dhp9RN4_fCc',
+  authDomain: 'react-chess-fdda6.firebaseapp.com',
+  projectId: 'react-chess-fdda6',
+  storageBucket: 'react-chess-fdda6.appspot.com',
+  messagingSenderId: '903307277231',
+  appId: '1:903307277231:web:2c448c962ed472f898f55a',
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
