45b30ce6d0f0d4efd0915e8235a721a1f11607b5
Serhii Karavashkin <ksawl@narod.ru>
2022-07-27 09:13:23 +0300

    Add routing and styling of the authorization form

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.scss';
-import BoardComponent from './components/BoardComponent';
-import HistoryComponent from './components/HistoryComponent';
+import Auth from './components/Auth';
+import Home from './components/Home';
+import { auth } from './firebase';
 
 function App() {
+  const user = auth.currentUser;
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate('/');
+    } else {
+      navigate('/auth');
+    }
+  }, [user, navigate]);
+
+  console.log('user: ', user);
   return (
     <div className="App">
-      <div className="board-wrap">
-        <BoardComponent />
-      </div>
-      <div className="history-wrap">
-        <HistoryComponent />
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Auth.tsx
@@ -0,0 +1,86 @@
+import React, { ChangeEvent } from 'react';
+import './Auth.scss';
+
+const Auth = () => {
+  const showPwd = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+
+    const pwdField = document.querySelector('input[name="pwd"]') as HTMLElement;
+    const button = e.target as HTMLElement;
+    if (button && pwdField) {
+      if (button.classList.contains('show')) {
+        button.classList.remove('show');
+        pwdField.setAttribute('type', 'password');
+      } else {
+        button.classList.add('show');
+        pwdField.setAttribute('type', 'text');
+      }
+
+      pwdField.focus();
+    }
+  };
+
+  const inputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const elem = e.target as HTMLInputElement;
+    elem.setAttribute('value', elem.value);
+  };
+
+  return (
+    <div className="auth">
+      <div className="panel">
+        <form action="post" className="form">
+          <div className="form-row">
+            <div className="form-field-wrap">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                onChange={inputChange}
+                tabIndex={1}
+              />
+              <label htmlFor="email">Email</label>
+            </div>
+          </div>
+          <div className="form-row">
+            <div className="form-field-wrap">
+              <input
+                type="password"
+                name="pwd"
+                id="pwd"
+                onChange={inputChange}
+                tabIndex={2}
+              />
+              <label htmlFor="pwd">Password</label>
+              <button
+                className="password"
+                onClick={showPwd}
+                tabIndex={3}
+              ></button>
+            </div>
+          </div>
+          <div className="form-row">
+            <button type="submit" tabIndex={4}>
+              Auth me
+            </button>
+          </div>
+          <div className="form-row">
+            <div className="forgot">
+              Forgot password?{' '}
+              <a href="/forgot" tabIndex={5}>
+                Restore it!
+              </a>
+            </div>
+          </div>
+        </form>
+        <div className="separator">
+          <span>OR</span>
+        </div>
+        <button className="auth-google" tabIndex={6}>
+          Auth with Google
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
